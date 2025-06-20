1c21a9315058fbca534fecb0816582e5e9a76858
Ramzee <n3opiner@proton.me>
2025-06-20 19:04:28 +0200

    Added serialized name to models

--- app/src/main/java/com/example/data/dto/ConversationDtoItem.kt
+++ app/src/main/java/com/example/data/dto/ConversationDtoItem.kt
@@ -1,6 +1,9 @@
 package com.example.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class ConversationDtoItem(
+    @SerializedName("messages")
     val messageDtos: List<MessageDto>,
     val username: String
 )
