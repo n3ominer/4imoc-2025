f1360a8b71c7b7eb84f0c70642466bfee2f8c99d
Ramzee <n3opiner@proton.me>
2025-06-20 16:45:56 +0200

    Added GSON dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,19 +50,27 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
+    // UI
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // TEST
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    // NAVIGATION
     implementation(libs.androidx.navigation.compose)
 
+    // Retrofit
     implementation(libs.retrofit)
+    implementation(libs.gson)
 }
